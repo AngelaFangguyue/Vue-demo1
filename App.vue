dcb9a1ce87160e5d152e374dafbcf357b0bcf9f8
AngelaFang <1823476439@qq.com>
2020-09-29 23:47:21 +0800

    提交

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">vue app</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style scoped>
+div {
+  border: 2px solid greenyellow;
+}
+</style>
